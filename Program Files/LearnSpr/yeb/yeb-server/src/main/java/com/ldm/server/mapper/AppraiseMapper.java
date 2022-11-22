6b2a77f951d177929109b7dacebd4791b1e96b25
Rollors <18270387673@163.com>
2022-11-22 20:56:05 +0800

    新建模型

---
+++ Program Files/LearnSpr/yeb/yeb-server/src/main/java/com/ldm/server/mapper/AppraiseMapper.java
@@ -0,0 +1,16 @@
+package com.ldm.server.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ldm.server.pojo.Appraise;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author ldm
+ * @since 2022-11-22
+ */
+public interface AppraiseMapper extends BaseMapper<Appraise> {
+
+}
---
+++ Program Files/LearnSpr/yeb/yeb-server/src/main/java/com/ldm/server/service/IRoleService.java
@@ -0,0 +1,16 @@
+package com.ldm.server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ldm.server.pojo.Role;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author ldm
+ * @since 2022-11-22
+ */
+public interface IRoleService extends IService<Role> {
+
+}
